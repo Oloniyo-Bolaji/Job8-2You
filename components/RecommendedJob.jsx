af2f2b5fece36c9b76ec73cb8bcd6fee62f21583
unknown <user.name>
2025-07-03 02:36:52 +0100

    recommended jobs card changed

--- components/RecommendedJob.jsx
+++ components/RecommendedJob.jsx
@@ -5,15 +5,15 @@ import React from 'react'
 
 const RecommendedJob = ({ job, removeRecommended }) => {
   return (
-    <div style={{backgroundColor : "#ff6b35"}} className="rounded-[10px] px-[5px] py-[10px] h-[150px] w-full flex justify-evenly ">
+    <div style={{backgroundColor : "#007bff20"}} className="rounded-[10px] px-[5px] py-[10px] h-[150px] w-full flex justify-evenly ">
       <div>
-        <h3 className="font-bold text-[15px]">{job.title}</h3>
-        <p className="text-[14px]">{job.company}</p>
+        <h3 className="font-bold text-[15px] text-[#007bff]">{job.title}</h3>
+        <p className="text-[13px]">{job.company}</p>
         <p className="text-[14px]">{job.roleType}</p>
         <span className="text-[14px]">{job.salary}</span>
       </div>
       <div>
-        <button onClick={() => removeRecommended(job.id)}>
+        <button onClick={() => removeRecommended(job.id)} className='text-[red]'>
           <CgClose />
         </button>
       </div>
